6f0750606d3dae23937686344125e1eb5db0933f
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-01-06 19:47:21 +0330

    Added card pan to CustomerInfo

--- ZarinSharp/Types/ContactInfo.cs
+++ ZarinSharp/Types/ContactInfo.cs
@@ -9,10 +9,14 @@ namespace ZarinSharp.Types
         /// </summary>
         /// <param name="mobile">Customer's mobile number</param>
         /// <param name="email">Customer's email address</param>
-        public CustomerInfo(string? mobile = default, string? email = default)
+        public CustomerInfo(
+            string? mobile = default,
+            string? email = default,
+            string? cardPan = default)
         {
             Mobile = mobile;
             Email = email;
+            CardPan = cardPan;
         }
 
         /// <summary>
@@ -26,5 +30,11 @@ namespace ZarinSharp.Types
         /// </summary>
         [JsonPropertyName("email")]
         public string? Email { get; }
+
+        /// <summary>
+        /// Customer's card pan - This forces customer to use this card for payment.
+        /// </summary>
+        [JsonPropertyName("card_pan")]
+        public string? CardPan { get; }
     }
 }
