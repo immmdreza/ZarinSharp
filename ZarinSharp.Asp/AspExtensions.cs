6a4737215c6e12b5f4507298e039c115adbc543a
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-01-07 13:22:35 +0330

    Created zarinsharp Asp

---
+++ ZarinSharp.Asp/AspExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ZarinSharp.Types;
+
+namespace ZarinSharp.Asp
+{
+    public static class AspExtensions
+    {
+        /// <summary>
+        /// Adds <see cref="IZarinClient"/> to this <see cref="IServiceCollection"/>
+        /// </summary>
+        /// <param name="zarinpalConfiguration">Zarinpal configuration.</param>
+        public static void AddZarinSharp(this IServiceCollection services, ZarinpalConfiguration zarinpalConfiguration)
+            => services.AddHttpClient("ZarinSharpClient")
+                .AddTypedClient<IZarinClient>(httpClient => new ZarinClient(
+                    zarinpalConfiguration, httpClient));
+
+        /// <summary>
+        /// Gets <see cref="ZarinpalConfiguration"/> from configs file (appsettings.json)
+        /// </summary>
+        public static ZarinpalConfiguration GetZarinpalConfiguration(this IConfiguration configuration)
+            => configuration.GetSection("ZarinpalConfiguration").Get<ZarinpalConfiguration>();
+    }
+}
